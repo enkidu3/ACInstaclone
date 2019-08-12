a91442766bc9b14ee61b861747363d88089f5b75
David Lynch <enkidu_3@yahoo.com>
2019-08-12 12:45:26 -0400

    first commit

---
+++ app/src/main/java/com/davidm/lynch/acinstaclone/LogInPage.java
@@ -0,0 +1,107 @@
+package com.davidm.lynch.acinstaclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+
+public class LogInPage extends AppCompatActivity implements View.OnClickListener{
+
+//    ParseUser user;
+    Button btnSignUp, btnLogIn;
+    EditText edtUsername, edtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_page);
+
+        // Save the current Installation to Back4App
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        setTitle("Log In");
+
+        edtUsername = findViewById(R.id.edtLIUsername);
+        edtPassword = findViewById(R.id.edtLIPassword);
+        btnLogIn = findViewById(R.id.btnLILogIn);
+        btnSignUp = findViewById(R.id.btnLISignUp);
+        btnSignUp.setOnClickListener(this);
+        btnLogIn.setOnClickListener(this);
+
+        edtPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(i == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_DOWN){
+                    onClick(btnSignUp);
+                }
+                return false;
+            }
+        });
+
+
+
+        if(ParseUser.getCurrentUser() != null){
+            ParseUser.getCurrentUser().logOut();
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        final String username = edtUsername.getText().toString();
+        String password = edtPassword.getText().toString();
+
+        switch(view.getId()){
+            case R.id.btnLISignUp:
+                if(ParseUser.getCurrentUser() != null){
+                    ParseUser.getCurrentUser().logOut();
+                }
+//                MainActivity.this.reset();
+//                findViewById(R.id.edtEmail).setText("");
+                finish();
+            break;
+            case R.id.btnLILogIn:
+                if (username.equals("")) {
+                    Toast.makeText(LogInPage.this, "Please enter a Username to proceed", Toast.LENGTH_LONG).show();
+                } else if (password.equals("")) {
+                    Toast.makeText(LogInPage.this, "Please enter a password to proceed", Toast.LENGTH_LONG).show();
+                } else {
+                    final ProgressDialog progressDialog= new ProgressDialog(this);
+                    progressDialog.setMessage(username + "is being logged in.");
+                    progressDialog.show();
+
+                    if(ParseUser.getCurrentUser() != null){
+                        ParseUser.getCurrentUser().logOut();
+                    }
+
+                    ParseUser.logInInBackground(username, password, new LogInCallback() {
+                        @Override
+                        public void done(ParseUser user, ParseException e) {
+                            if(e !=null){
+                                Toast.makeText(LogInPage.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(LogInPage.this, username+ " is logged in.", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
+                    progressDialog.dismiss();
+                 }
+             break;
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/davidm/lynch/acinstaclone/MainActivity.java
@@ -0,0 +1,111 @@
+package com.davidm.lynch.acinstaclone;
+
+
+
+// Player1 123, Player2 12345, User password
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btnSignUp, btnLogIn;
+    private EditText edtUserName, edtPassword, edtEmail;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Save the current Installation to Back4App
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        setTitle("Sign Up");
+
+        btnLogIn = findViewById(R.id.btnLogIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtUserName = findViewById(R.id.edtUsername);
+        btnSignUp.setOnClickListener(this);
+        btnLogIn.setOnClickListener(this);
+
+        edtPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(i == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_DOWN){
+                    onClick(btnSignUp);
+                }
+                return false;
+            }
+        });
+
+        if(ParseUser.getCurrentUser() != null){
+            ParseUser.getCurrentUser().logOut();
+        }
+
+    }
+
+    public void reset() {
+        edtUserName.setText("");
+        edtPassword.setText("");
+        edtEmail.setText("");
+
+    }
+    @Override
+    public void onClick(View view) {
+        String email = edtEmail.getText().toString();
+        final String username = edtUserName.getText().toString();
+        String password = edtPassword.getText().toString();
+        if (view == btnLogIn){
+            //switch to Log In Page
+            Intent intent = new Intent(MainActivity.this, LogInPage.class);
+            startActivity(intent);
+            reset();
+        }
+        else if(view == btnSignUp){
+            if (email.equals("")){
+                Toast.makeText(MainActivity.this, "Please enter an email to proceed", Toast.LENGTH_LONG).show();
+            }else if (username.equals("")){
+                Toast.makeText(MainActivity.this, "Please enter a username to proceed", Toast.LENGTH_LONG).show();
+            }else if (password.equals("")){
+                Toast.makeText(MainActivity.this, "Please enter a password to proceed", Toast.LENGTH_LONG).show();
+            }else {
+                final ParseUser user = new ParseUser();
+                user.setEmail(email);
+                user.setUsername(username);
+                user.setPassword(password);
+
+                final ProgressDialog progressDialog= new ProgressDialog(this);
+                progressDialog.setMessage(username + "is being signed up.");
+                progressDialog.show();
+
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+                            Toast.makeText(MainActivity.this,  "Welcome, " + username + ".  You are now signed in", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+            }
+        }
+
+    }
+}
